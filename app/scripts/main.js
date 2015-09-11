cb590e9a0be55376784732c1a5c7210ac9635382
lacostenycoder <djlancejordan@gmail.com>
2015-09-11 03:07:53 -0400

    basic nav working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,22 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $(".nav a").on("click", function(){
+    var $active = $(".nav").find(".active");
+     $active.removeClass("active");
+     $(this).parent().addClass("active");
+     var $el = $(this);
+     navSelector($el.attr('id'));
+  });
+
+  function navSelector(unHide){
+    var pages = $('.page-section');
+    $.each(pages, function(){
+      if(this.className.split(' ')[1] != "hidden"){
+        $(this).addClass('hidden');
+      }
+    });
+    $('#'+unHide+'-page').removeClass('hidden');
+  }
+
+  $('#home').click();
+});
