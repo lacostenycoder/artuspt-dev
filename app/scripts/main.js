2c9432bfeaafa0b0721070e9a334dc35028a6ae0
lacostenycoder <djlancejordan@gmail.com>
2015-09-12 02:22:49 -0400

    add top info bar. slider working. add breadcrumbs for about pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,35 @@
 // jshint devel:true
 $(document).ready(function(){
+
+// navbar unhides selected items
   $(".nav a").on("click", function(){
     var $active = $(".nav").find(".active");
      $active.removeClass("active");
      $(this).parent().addClass("active");
      var $el = $(this);
      navSelector($el.attr('id'));
+     showSlider($el.attr('id'));
   });
+//  breadcrumb used for about pages
+  $(".breadcrumb a").on("click", function(){
+    var $active = $(".breadcrumb").find(".active");
+     $active.removeClass("active");
+     $(this).parent().addClass("active");
+     var $el = $(this);
+    //  console.log($el.attr('id'));
+     breadcrumbSelector($el.attr('id'));
+  });
+
+  function breadcrumbSelector(unHide){
+    var abouts = $('.about');
+    console.log(abouts[0]);
+    $.each(abouts, function(){
+      if(this.className.split(' ')[0] != "hidden"){
+        $(this).addClass('hidden');
+      }
+    });
+    $('#'+'about-'+unHide).removeClass('hidden');
+  }
 
   function navSelector(unHide){
     var pages = $('.page-section');
@@ -18,5 +41,44 @@ $(document).ready(function(){
     $('#'+unHide+'-page').removeClass('hidden');
   }
 
+// show slider on home page only
+  function showSlider(activeTab){
+    if(activeTab != "home"){
+      $('.slider').hide();
+    } else {
+      $('.slider').show();
+    }
+  }
+
   $('#home').click();
+
+  // init slider
+  function initSlider() {
+    var options = { $AutoPlay: true };
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+
+    //responsive code begin
+      //you can remove responsive code if you don't want the slider scales
+      //while window resizes
+      function ScaleSlider() {
+          var parentWidth = $('#slider1_container').parent().width();
+          if (parentWidth) {
+              jssor_slider1.$ScaleWidth(parentWidth);
+          }
+          else
+              window.setTimeout(ScaleSlider, 30);
+      }
+      //Scale slider after document ready
+      ScaleSlider();
+
+      //Scale slider while window load/resize/orientationchange.
+      $(window).bind("load", ScaleSlider);
+      $(window).bind("resize", ScaleSlider);
+      $(window).bind("orientationchange", ScaleSlider);
+      //responsive code end
+
+  }
+
+  initSlider();
+
 });
